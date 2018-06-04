5c78ca01f9217d10dc9e9d633c9fa9fd2efb9458
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-04 13:33:59 -0600

    Completes user story 1 less BOSS MODE

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,6 @@ class SearchController < ApplicationController
     search = LocationDetail.new(params[:search])
     @top_cuisines = search.location_details_body['top_cuisines']
     @top_5_restaurants = search.location_details_body['best_rated_restaurant'][0..4]
-    binding.pry
+    # binding.pry
   end
 end
-@top_5_restaurants[0]["restaurant"]['name']
-@top_5_restaurants[0]["restaurant"]['location']['address']
-@top_5_restaurants[0]["restaurant"]['location']['locality']
