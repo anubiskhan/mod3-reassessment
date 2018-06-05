041707a3658cd3dc7fb490c8091ff00a4f720b8b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 16:45:14 -0600

    Removes things that were mid refactor

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,19 +1,13 @@
 class LocationService
   def initialize(location)
-    @location = location
-    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/")
+    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/locations?query=#{location}")
   end
 
   def location_body
-    locations?query=#{location}
     resp = @conn.get do |conn|
       conn.headers['user-key'] = ENV['API_KEY']
     end
 
     JSON.parse(resp.env.body)
-  end
-
-  def response
-    
   end
 end
