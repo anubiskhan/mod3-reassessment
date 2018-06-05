082ea397e4f5873a863e4a964ab90df9867ae3de
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 16:44:25 -0600

    Fixes file names

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,13 +1,19 @@
 class LocationService
   def initialize(location)
-    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/locations?query=#{location}")
+    @location = location
+    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/")
   end
 
   def location_body
+    locations?query=#{location}
     resp = @conn.get do |conn|
       conn.headers['user-key'] = ENV['API_KEY']
     end
 
     JSON.parse(resp.env.body)
+  end
+
+  def response
+    
   end
 end
