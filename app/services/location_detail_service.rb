7337d10556a60e89c2a8e268fb8efc7d0004131d
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-05 16:53:55 -0600

    Services refactor

--- app/services/location_detail_service.rb
+++ app/services/location_detail_service.rb
@@ -1,13 +1,19 @@
 class LocationDetailService
   def initialize(type, id)
-    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/location_details?entity_id=#{id}&entity_type=#{type}")
+    @type = type
+    @id = id
+    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/location_details")
   end
 
   def location_detail_body
-    resp = @conn.get do |conn|
-      conn.headers['user-key'] = ENV['API_KEY']
-    end
+    JSON.parse(response.body)
+  end
 
-    JSON.parse(resp.body)
+  def response
+    @conn.get do |conn|
+      conn.headers['user-key'] = ENV['API_KEY']
+      conn.params['entity_type'] = @type
+      conn.params['entity_id'] = @id
+    end
   end
 end
--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,13 +1,17 @@
 class LocationService
   def initialize(location)
-    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/locations?query=#{location}")
+    @location = location
+    @conn = Faraday.new(url: "https://developers.zomato.com/api/v2.1/locations")
   end
 
   def location_body
-    resp = @conn.get do |conn|
-      conn.headers['user-key'] = ENV['API_KEY']
-    end
+    JSON.parse(response.env.body)
+  end
 
-    JSON.parse(resp.env.body)
+  def response
+    @conn.get do |conn|
+      conn.headers['user-key'] = ENV['API_KEY']
+      conn.params['query'] = @location
+    end
   end
 end
