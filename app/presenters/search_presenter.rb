d52a70bd85e9ef152fb7af92a6ca59a55c005b8c
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-06 12:19:29 -0600

    Changes search presenter to produce an array of cuisine objects instead of strings

--- app/presenters/search_presenter.rb
+++ app/presenters/search_presenter.rb
@@ -7,12 +7,14 @@ class SearchPresenter
   end
 
   def top_cuisines
-    @location_detail_service.location_detail_body['top_cuisines']
+    @location_detail_service.location_detail_body['top_cuisines'].map do |cuisine|
+      cuisinerate(cuisine)
+    end
   end
 
   def top_5_restaurants
-    @location_detail_service.location_detail_body['best_rated_restaurant'][0..4].map do |r|
-      restaurantify(r)
+    @location_detail_service.location_detail_body['best_rated_restaurant'][0..4].map do |restaurant|
+      restaurantify(restaurant)
     end
   end
 
@@ -20,5 +22,9 @@ class SearchPresenter
 
   def restaurantify(restaurant_hash)
     Restaurant.new(restaurant_hash)
+  end
+
+  def cuisinerate(cuisine)
+    Cuisine.new(cuisine)
   end
 end
